3208d415c666730491af76a82b3a513b54fd256b
Alexander Witt <92024276+Baumwollgarn@users.noreply.github.com>
2022-10-04 22:23:16 +0200

    Class for DB connector

---
+++ src/database/connectionManager.java
@@ -0,0 +1,28 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class connectionManager {
+    private static Connection con;
+
+    public static Connection getConnection() {
+        try {
+            String driverName = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driverName);
+            try {
+                String username = "root";
+                String password = "Alex2002";
+                String url = "jdbc:mysql://localhost:3306/Movies";
+                con = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+
+}
